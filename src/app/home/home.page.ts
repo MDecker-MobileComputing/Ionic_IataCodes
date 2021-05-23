d2aa2af83bcd6c06f2918ddf1a9fed2f7115391e
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-05-23 21:18:52 +0200

    Eingabefeld und zwei Buttons erstellt; AndroStudio-Projekt erzeugt.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private alertCtrl: AlertController) {}
+
+  /**
+   * Event-Handler-Methode für Button "Flughafen suchen".
+   */
+  public onFlughafenSuchen() {
+
+    this.zeigeDialog("Info", "Not implemented yet :-(");
+  }
+
+  /**
+   * Event-Handler-Methode für Button "Fluggesellschaft suchen".
+   */
+  public onFluggesellschaftSuchen() {
+
+    this.zeigeDialog("Info", "Not implemented yet :-(");
+  }
+
+  /**
+   * Hilfsmethode: Alert anzeigen, siehe auch https://ionicframework.com/docs/api/alert
+   *
+   * @param titel Titel des Dialogs
+   * @param nachricht Im Dialog anzuzeigender Text
+   */
+   async zeigeDialog(titel: string, nachricht: string) {
+
+    const meinAlert =
+          await this.alertCtrl.create({
+              header  : titel,
+              message : nachricht,
+              buttons : [ "Ok" ]
+          });
+
+    await meinAlert.present();
+  }
 
 }
