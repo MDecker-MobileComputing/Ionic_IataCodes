d77946eae15be3683f6dcece9c4b332e1d40107f
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-05-24 19:36:26 +0200

    Mehrere Testfaelle fuer sucheFlughafen()

--- src/app/iatadb.service.spec.ts
+++ src/app/iatadb.service.spec.ts
@@ -13,4 +13,68 @@ describe('IatadbService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Flughafen "xxx" nicht gefunden', () => {
+
+    const flughafenErgebnis = service.sucheFlughafen("xxx");
+
+    expect(flughafenErgebnis.gefunden).toBeFalse()
+
+    expect(flughafenErgebnis.flughafen).toEqual("");
+    expect(flughafenErgebnis.land     ).toEqual("");
+  });
+
+  it('Flughafen "FRA" gefunden', () => {
+
+    const flughafenErgebnis = service.sucheFlughafen("FRA");
+
+    expect(flughafenErgebnis.gefunden).toBeTrue();
+
+    expect(flughafenErgebnis.flughafen).toEqual("Frankfurt a.M.");
+    expect(flughafenErgebnis.land     ).toEqual("Deutschland");
+  });
+
+  it('Flughafen "fra" gefunden', () => {
+
+    const flughafenErgebnis = service.sucheFlughafen("fra");
+
+    expect(flughafenErgebnis.gefunden).toBeTrue();
+
+    expect(flughafenErgebnis.flughafen).toEqual("Frankfurt a.M.");
+    expect(flughafenErgebnis.land     ).toEqual("Deutschland");
+  });
+
+  it('Kein Flughafen für zu kurzen Code "ab" gefunden', () => {
+
+    const flughafenErgebnis = service.sucheFlughafen("ab");
+
+    expect(flughafenErgebnis.gefunden).toBeFalse()
+
+    expect(flughafenErgebnis.flughafen).toEqual("");
+    expect(flughafenErgebnis.land     ).toEqual("");
+  });
+
+  it('Kein Flughafen für zu langen Code "abcd" gefunden', () => {
+
+    const flughafenErgebnis = service.sucheFlughafen("abcd");
+
+    expect(flughafenErgebnis.gefunden).toBeFalse()
+
+    expect(flughafenErgebnis.flughafen).toEqual("");
+    expect(flughafenErgebnis.land     ).toEqual("");
+  });
+
+  it('Kein Flughafen für leeren String gefunden', () => {
+
+    const flughafenErgebnis = service.sucheFlughafen("");
+
+    expect(flughafenErgebnis.gefunden).toBeFalse()
+
+    expect(flughafenErgebnis.flughafen).toEqual("");
+    expect(flughafenErgebnis.land     ).toEqual("");
+  });
+
 });
+
+
+
