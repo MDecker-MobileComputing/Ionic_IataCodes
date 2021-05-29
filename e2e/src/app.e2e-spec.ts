b3ba176fca407b6c81597bc7e51747b8e24fc93d
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-05-29 20:36:18 +0200

    Erster Protractor-Testfall "Auf Startseite ist alles da" vollstaendig.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { AppPage } from './app.po';
 import { browser, by, element } from 'protractor';
+import { exception } from 'console';
 
 describe('new App', () => {
   let page: AppPage;
@@ -10,14 +11,27 @@ describe('new App', () => {
 
   // Pfeilfunktion mit "async" versehen und "await" vor page.navigateTo()
   // wegen diesem Issue: https://github.com/ionic-team/starters/issues/1669#issuecomment-784452288
-  it('Auf leerer Seite ist alles da.', async () => {
+  it('Auf Startseite ist alles da', async () => {
 
     await browser.get("/");
 
-    element(by.buttonText("Flughafen suchen"));
-    element(by.buttonText("Fluggesellschaft suchen"));
+    const button1 = element(by.id("buttonFlughafensuche"));    
+    const button1Angezeigt = await button1.isDisplayed();
+    expect(button1Angezeigt).toBe(true);
 
-    const eingabeFeld = element(by.binding("iataCode"));
-    expect(eingabeFeld.getText()).toEqual("");
+    const button2 = element(by.id("buttonAirlineSuche"));    
+    const button2Angezeigt = await button2.isDisplayed();
+    expect(button2Angezeigt).toBe(true);
+
+
+    const eingabeFeld = element(by.id("codeEingabe"));
+    const eingabeFeldAngezeigt = await eingabeFeld.isDisplayed();
+    expect(eingabeFeldAngezeigt).toBe(true);
+
+    const platzhalterTextEingabefeld = await eingabeFeld.getAttribute("placeholder");
+    expect(platzhalterTextEingabefeld).toEqual("Code für Flughafen oder Airline");
+
+    const inhaltTextEingabefeld = await eingabeFeld.getText();
+    expect(inhaltTextEingabefeld).toEqual("");
   });
 });
