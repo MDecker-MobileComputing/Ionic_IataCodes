c3bd15448ebd30fd44c1cd4a2afeb5906d875cb7
MDecker-MobileComputing <mide_42b@yahoo.de>
2022-05-28 13:57:39 +0200

    Android-Studio-Projekt neu erzeugt (nach Upgrade auf Ionic 6).

--- src/app/fluggesellschaftergebnis.spec.ts
+++ src/app/fluggesellschaftergebnis.spec.ts
@@ -1,6 +1,7 @@
 import { Fluggesellschaftergebnis } from './fluggesellschaftergebnis';
 
 describe('Fluggesellschaftergebnis', () => {
+
   it('should create an instance', () => {
 
     expect(new Fluggesellschaftergebnis(false, "", "")).toBeTruthy();
--- src/app/flughafenergebnis.spec.ts
+++ src/app/flughafenergebnis.spec.ts
@@ -1,6 +1,7 @@
 import { Flughafenergebnis } from './flughafenergebnis';
 
 describe('Flughafenergebnis', () => {
+
   it('should create an instance', () => {
     expect(new Flughafenergebnis(false, "", "")).toBeTruthy();
   });
